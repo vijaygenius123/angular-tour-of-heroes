9d88288c8bd18632ab0503e619b3aeaafbef33ab
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-24 22:17:34 +0530

    Integratred message service in app

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero";
-import {HeroService} from "../../hero.service";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -11,6 +11,7 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
   selectedHero: Hero;
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
