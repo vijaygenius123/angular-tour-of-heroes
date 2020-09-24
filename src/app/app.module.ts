f8bea5b1059a989c7b6a0c82298f6a2820d7b619
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-24 22:11:49 +0530

    Created message component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
