b1a6c84a55e6753fdcd61ece8ea20720e24141a1
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-25 20:18:21 +0530

    Added Dashboard And Configured Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HeroesComponent } from './heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
